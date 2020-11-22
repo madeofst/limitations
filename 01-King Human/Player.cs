77e729fa69d95013c648f9d7c824230747fc3f04
madeofst <mikemccabe85@gmail.com>
2020-11-22 23:30:54 +0000

    updated running anim

--- 01-King Human/Player.cs
+++ 01-King Human/Player.cs
@@ -3,29 +3,44 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    private int ACCELERATION = 800;
-    public int MAXSPEED = 300;
+    private int ACCELERATION = 850;
+    private int MAXSPEED = 325;
+    private int FRICTION = 1200;
 
     private Vector2 velocity = new Vector2(Vector2.Zero);
     private AnimationPlayer aPlayer;
 
+    public override void _Ready()
+    {
+        aPlayer = GetNode<AnimationPlayer>("PlayerAnimationPlayer");
+    }
+
     public override void _PhysicsProcess(float delta)
     {
         Vector2 inputVector = new Vector2(Vector2.Zero);
-        aPlayer = GetNode<AnimationPlayer>("PlayerAnimationPlayer");
-
         inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-        velocity = velocity.MoveToward(inputVector * MAXSPEED, delta * ACCELERATION);
+
         if(inputVector.x != 0)
         {
-            aPlayer.Play("IdleRight");
+            if (inputVector.x > 0){
+                aPlayer.Play("RunRight");
+            }
+            else{
+                aPlayer.Play("RunLeft");
+            }
+            velocity = velocity.MoveToward(inputVector * MAXSPEED, delta * ACCELERATION);
         }
         else
         {
-            
+            if (velocity.x > 0){
+                aPlayer.Play("IdleRight");
+            }
+            else if (velocity.x < 0){
+                aPlayer.Play("IdleLeft");
+            }
+            velocity = velocity.MoveToward(Vector2.Zero, delta * FRICTION);
         }
         velocity = MoveAndSlide(velocity);
-        //GD.Print(velocity.x);
 
         //Node2D world = GetParent<Node2D>();
         //GD.Print(world.Name);

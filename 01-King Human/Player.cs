5a0c16d0257ecceeb31cf7b6d359a69b9be1fb3d
madeofst <mikemccabe85@gmail.com>
2020-11-26 15:27:53 +0000

    updated jump etc.

--- 01-King Human/Player.cs
+++ 01-King Human/Player.cs
@@ -4,46 +4,69 @@ using System;
 public class Player : KinematicBody2D
 {
     private int ACCELERATION = 850;
-    private int MAXSPEED = 325;
+    private int MAXSPEED = 275;
     private int FRICTION = 1200;
+    private int GRAVITYSTRENGTH = 1250;
+    private int JUMPSTRENGTH = -250;
+    private int JUMPACCELERATION = 5000;
 
+    private Vector2 baselineGravity;
+    private Vector2 horizontalDirection = new Vector2(Vector2.Zero);
     private Vector2 velocity = new Vector2(Vector2.Zero);
-    private AnimationPlayer aPlayer;
+    private AnimationPlayer animationPlayer;
+    private bool jumping;
 
     public override void _Ready()
-    {
-        aPlayer = GetNode<AnimationPlayer>("PlayerAnimationPlayer");
+    { 
+        animationPlayer = GetNode<AnimationPlayer>("PlayerAnimationPlayer");
+        horizontalDirection = Vector2.Right;
+        baselineGravity = new Vector2(0,GRAVITYSTRENGTH);
+        velocity = baselineGravity;
     }
 
     public override void _PhysicsProcess(float delta)
     {
         Vector2 inputVector = new Vector2(Vector2.Zero);
+        //inputVector.x = 1;
         inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        if (Input.IsActionJustPressed("ui_jump") & IsOnFloor())
+        //if (IsOnFloor())
+        {
+            jumping = true;
+        }
+        else if (Input.IsActionJustReleased("ui_jump") | IsOnFloor() | velocity.y <= JUMPSTRENGTH + (delta * GRAVITYSTRENGTH))
+        {
+            jumping = false;
+        }
+        if (jumping) {velocity.y = velocity.MoveToward(Vector2.Down * JUMPSTRENGTH, delta * JUMPACCELERATION).y;}
+        velocity.y = velocity.MoveToward(baselineGravity, delta * GRAVITYSTRENGTH).y;
 
         if(inputVector.x != 0)
         {
+            horizontalDirection = inputVector;
             if (inputVector.x > 0){
-                aPlayer.Play("RunRight");
+                animationPlayer.Play("RunRight");
             }
-            else{
-                aPlayer.Play("RunLeft");
+            else if(inputVector.x < 0){
+                animationPlayer.Play("RunLeft");
             }
-            velocity = velocity.MoveToward(inputVector * MAXSPEED, delta * ACCELERATION);
+            velocity.x = velocity.MoveToward(inputVector * MAXSPEED, delta * ACCELERATION).x;
         }
         else
         {
-            if (velocity.x > 0){
-                aPlayer.Play("IdleRight");
+            if (horizontalDirection.x > 0){
+                animationPlayer.Play("IdleRight");
             }
-            else if (velocity.x < 0){
-                aPlayer.Play("IdleLeft");
+            else if (horizontalDirection.x < 0){
+                animationPlayer.Play("IdleLeft");
             }
-            velocity = velocity.MoveToward(Vector2.Zero, delta * FRICTION);
+            velocity.x = velocity.MoveToward(Vector2.Zero, delta * FRICTION).x;
         }
-        velocity = MoveAndSlide(velocity);
+
+        velocity = MoveAndSlide(velocity,Vector2.Up);
 
         //Node2D world = GetParent<Node2D>();
-        //GD.Print(world.Name);
+        GD.Print("");
     }
 
 }
